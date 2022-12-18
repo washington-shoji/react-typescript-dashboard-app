3f2f08a5a57616dae2e46f4f0a427ae19f892399
washington-shoji <shoji.washington@gmail.com>
2022-12-18 21:30:08 +1100

    feat(style): added carto map style

--- src/enum/mapstyle.ts
+++ src/enum/mapstyle.ts
@@ -1,4 +1,5 @@
 export enum MAP_BOX_STYLE {
 	MapBoxDark = 'mapbox://styles/mapbox/dark-v11',
 	MapBoxLight = 'mapbox://styles/mapbox/light-v11',
+	CartoDark = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json',
 }
