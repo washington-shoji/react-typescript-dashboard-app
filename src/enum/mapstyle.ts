76573811583ba06ced8249996950de28f37ccd52
washington-shoji <shoji.washington@gmail.com>
2023-01-05 18:50:21 +1100

    feat(style ): updated map styles

--- src/enum/mapstyle.ts
+++ src/enum/mapstyle.ts
@@ -1,5 +1,7 @@
 export enum MAP_BOX_STYLE {
 	MapBoxDark = 'mapbox://styles/mapbox/dark-v11',
 	MapBoxLight = 'mapbox://styles/mapbox/light-v11',
+	MapBoxStreet = 'mapbox://styles/mapbox/streets-v12',
+	MapBoxOutdoors = 'mapbox://styles/mapbox/outdoors-v12',
 	CartoDark = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json',
 }
