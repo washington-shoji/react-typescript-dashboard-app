9064e3563b03a0d11af0b148ef495d6e36f4ec73
washington-shoji <shoji.washington@gmail.com>
2023-01-05 18:48:53 +1100

    feat(assets): added map legend images

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import street from './images/street.png';
+import outdoors from './images/outdoors.png';
+import light from './images/light.png';
+import dark from './images/dark.png';
+
+export { street, outdoors, light, dark };
