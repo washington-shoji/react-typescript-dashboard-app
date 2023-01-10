d66a46a3fd77f1bf0bed1a3159ea5da6964ff6f4
washington-shoji <shoji.washington@gmail.com>
2023-01-11 08:02:17 +1100

    feat(component): sidebar component

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,45 +1,44 @@
-import { useState } from 'react';
 import './Sidebar.scss';
-import { NavLink } from 'react-router-dom';
-import { ISidebarData, menuData } from './sidebar-data';
-export default function Sidebar() {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
-	function toggleNavbar(): void {
-		setIsOpen(!isOpen);
-	}
+import { FaWindowClose } from 'react-icons/fa';
+import { sidebarMenuData } from '../sidebar-menu/sidebarMenu';
+
+export default function Sidebar({
+	isOpen,
+	toggleNavbar,
+	selectedMenu,
+}: {
+	isOpen: boolean;
+	toggleNavbar: () => void;
+	selectedMenu: string;
+}) {
 	return (
 		<div
-			className={
-				isOpen ? 'sidebar-container flex' : 'sidebar-container flex closed'
-			}
+			className={isOpen ? 'sidebar-container' : 'sidebar-container close'}
+			onClick={toggleNavbar}
 		>
-			<div className='logo-container flex' onClick={toggleNavbar}>
-				<img
-					src='https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'
-					alt=''
-				/>
-				<h2>JSD</h2>
-			</div>
-			<div className='menu-container flex'>
-				<ul className='menu-list grid'>
-					{menuData.map((item: ISidebarData, index: number) => {
-						const { Icon, name, link } = item;
-						return (
-							<NavLink
-								key={index}
-								to={link}
-								className={({ isActive }) =>
-									isActive ? `menu-list-item active` : `menu-list-item`
-								}
-							>
-								<li className='menu-link flex' key={item.id}>
-									<Icon className='icon' />
-									<span className='menu-text'>{name}</span>
-								</li>
-							</NavLink>
-						);
+			<div
+				className='sidebar-container-content'
+				onClick={(event) => {
+					// do not close modal if anything inside modal content is clicked
+					event.stopPropagation();
+				}}
+			>
+				<div className='sidebar-header'>
+					<span>XPL Map</span>
+					<FaWindowClose
+						className='sidebar-close-icon'
+						onClick={toggleNavbar}
+					/>
+				</div>
+				<div className='sidebar-body'>
+					{sidebarMenuData.map((component) => {
+						if (component.name === selectedMenu) {
+							const Component = component.component;
+							return <Component key={component.id} />;
+						}
+						return <div key={component.id}></div>;
 					})}
-				</ul>
+				</div>
 			</div>
 		</div>
 	);
